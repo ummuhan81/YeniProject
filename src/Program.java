96367eb06c3d15b1fff6b0df67d478fb3d1fef49
ummuhancebeci <ummuhanalgl81@gmail.com>
2022-10-13 10:21:35 +0200

    project has been created.

---
+++ src/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("yeni projeden herkese merhaba");
+        System.out.println("-----------------------");
+
+    }
+}
